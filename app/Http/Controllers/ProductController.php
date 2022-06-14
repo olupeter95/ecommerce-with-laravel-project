618e1a60db22c0310de331d74aab5cc762c5a791
Oluwasola peter <52799097+olupeter95@users.noreply.github.com>
2022-06-14 10:42:41 +0100

    product update data

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -135,5 +135,66 @@ class ProductController extends Controller
     }
 
 
-    
+    public function updateProductData(Request $request){
+        $validateData =$request->validate([
+            'brand_id'=>'required',
+            'category_id'=>'required',
+            'subcategory_id'=>'required',
+            'subsubcategory_id'=>'required',
+            'product_name_en'=>'required',
+            'product_name_fr'=>'required',
+            'product_code'=>'required',
+            'product_qty'=>'required',
+            'product_tags_en'=>'required',
+            'product_tags_fr'=>'required',
+            'product_size_en'=>'required',
+            'product_size_fr'=>'required',
+            'product_color_en'=>'required',
+            'product_color_fr'=>'required',
+            'selling_price'=>'required',
+            'short_desc_en'=>'required',
+            'short_desc_fr'=>'required',
+            'description_en'=>'required',
+            'description_fr'=>'required',
+        ]);
+
+        $id = $request->id;
+            $prdouct_id = Product::findorFail($id)->update([
+            'brand_id'=>$request->brand_id,
+            'category_id'=>$request->category_id,
+            'subcategory_id'=>$request->subcategory_id,
+            'subsubcategory_id'=>$request->subsubcategory_id,
+            'product_name_en'=>$request->product_name_en,
+            'product_name_fr'=>$request->product_name_fr,
+            'product_slug_en'=> strtolower(str_replace(" ","-",$request->product_name_en)),
+            'product_slug_fr'=>strtolower(str_replace(" ","-",$request->product_name_fr)),
+            'product_code'=>$request->product_code,
+            'product_qty'=>$request->product_qty,
+            'product_tags_en'=>$request->product_tags_en,
+            'product_tags_fr'=>$request->product_tags_fr,
+            'product_size_en'=>$request->product_size_en,
+            'product_size_fr'=>$request->product_size_fr,
+            'product_color_en'=>$request->product_color_en,
+            'product_color_fr'=>$request->product_color_fr,
+            'selling_price'=>$request->selling_price,
+            'discount_price'=>$request->discount_price,
+            'short_desc_en'=>$request->short_desc_en,
+            'short_desc_fr'=>$request->short_desc_fr,
+            'description_en'=>$request->description_en,
+            'description_fr'=>$request->description_fr,
+            'featured'=>$request->featured,
+            'hot_deals'=>$request->hot_deals,
+            'special_offer'=>$request->special_offer,
+            'special_deals'=>$request->special_deals,
+            'status'=>1,
+            'created_at' => Carbon::now(), 
+        ]);
+
+        $notification = array(
+            'message' => 'product data updated successfully',
+            'alert-type'=> 'success'
+         );
+         return redirect()->route('view.product')->with($notification);
+        
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -111,5 +111,6 @@ Route::prefix('product')->group(function(){
     Route::get('/manage',[ProductController::class,'viewProduct'])->name('view.product')->middleware('auth:admin');
     Route::post('/store',[ProductController::class,'addProduct'])->name('store.product');
     Route::get('/edit/{id}',[ProductController::class,'editProduct'])->name('edit.product')->middleware('auth:admin');
-    Route::get('/delete//{id}',[ProductController::class,'delProduct'])->name('delete.product');
+    Route::get('/delete/{id}',[ProductController::class,'delProduct'])->name('delete.product');
+    Route::post('/data/update',[ProductController::class,'updateProductData'])->name('update.product');
 });
